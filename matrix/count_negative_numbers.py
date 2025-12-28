cafabb28a25c5e0ef97add3fe63c0544f2e06819
1Viren <springvent096@gmail.com>
2025-12-28 15:11:33 +0200

    Add optimized solution for count negative numbers in matrix

---
+++ matrix/count_negative_numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countNegatives(self, grid: list[list[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        r, c = rows - 1, 0
+        count = 0
+        while r >= 0 and c < cols:
+            if grid[r][c] < 0:
+                count += cols - c
+                r -= 1
+            else:
+                c += 1
+        return count
